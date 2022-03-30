ad5281e4c76dfb08149ab9923213f93e0182709c
S1bery <45617892+S1bery@users.noreply.github.com>
2022-03-30 17:23:02 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+int main() {
+    int nums[5][5];
+
+    int  sequence = 0;
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 5; j++) {
+            nums[i][j] = sequence;
+            sequence += 1;
+        }
+    }
+
+    for (int i = 0; i < 5; i++) {
+        if (i % 2 == 0) {
+            for (int j = 0; j < 5; j++) {
+                std::cout << nums[i][j] << " ";
+            }
+        } else if (i % 2 != 0) {
+            for (int j = 4; j >= 0; j--) {
+                std::cout << nums[i][j] << " ";
+            }
+        }
+        std::cout << std::endl;
+    }
+
+    return 0;
+}
