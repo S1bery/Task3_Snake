724830adf2ee949ae1edf3ed0798c4de3e9654ba
S1bery <45617892+S1bery@users.noreply.github.com>
2022-04-04 22:59:08 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,23 +3,26 @@
 int main() {
     int nums[5][5];
 
-    int  sequence = 0;
+    int sequence = 0;
     for (int i = 0; i < 5; i++) {
-        for (int j = 0; j < 5; j++) {
-            nums[i][j] = sequence;
-            sequence += 1;
+        if (i % 2 == 0) {
+            for (int j = 0; j < 5; j++) {
+                nums[i][j] = sequence;
+                sequence += 1;
+            }
+        } else {
+            sequence += 4;
+                for (int j = 0; j < 5; j++) {
+                    nums[i][j] = sequence;
+                    sequence -= 1;
+            }
+            sequence += 6;
         }
     }
 
     for (int i = 0; i < 5; i++) {
-        if (i % 2 == 0) {
-            for (int j = 0; j < 5; j++) {
-                std::cout << nums[i][j] << " ";
-            }
-        } else if (i % 2 != 0) {
-            for (int j = 4; j >= 0; j--) {
-                std::cout << nums[i][j] << " ";
-            }
+        for (int j = 0; j < 5; j++) {
+            std::cout << nums[i][j] << " ";
         }
         std::cout << std::endl;
     }
